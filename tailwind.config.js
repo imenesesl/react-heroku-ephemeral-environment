f681c6589509ef7bb8680b4105f40aba199ed9bf
Luis Meneses <luismenesesep@gmail.com>
2024-01-17 15:41:51 -0500

    globals defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   mode: 'jit',
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      }
+    }
   },
   plugins: []
 };
