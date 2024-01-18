116daf47ea189527d28cef3c9459de22e55238db
Luis Meneses <luismenesesep@gmail.com>
2024-01-17 23:21:43 -0500

    core: update moving references

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@root/App.tsx';
-import '@root/index.scss';
+import '@root/style/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
