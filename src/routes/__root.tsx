2737d343e67d53ae332fc217cef905bb08770b3c
Luis Meneses <luismenesesep@gmail.com>
2024-01-20 01:01:15 -0500

    fix added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,18 +2,23 @@ import { Outlet, RootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import { Scaffold } from '@components/scaffold';
+import { Logger } from '@modules/logger';
 import { Tracker } from '@modules/tracker';
+import { ICallbackArgs } from '@modules/tracker/types';
 
 const tracker = new Tracker(import.meta.env.VITE_SEGMENT__WRITE_KEY);
+const logger = new Logger('@Root');
 
 export const Route = new RootRoute({
   beforeLoad: ({ cause, context, location, params, preload, search }) => {
-    tracker.track({
+    const payload: ICallbackArgs = {
       event: 'before-load',
       properties: { cause, context, location, params, preload, search },
       type: 'track',
       userId: 'user-id'
-    });
+    };
+    if (import.meta.env.PROD) tracker.track(payload);
+    else logger.log('before-load', payload);
   },
   component: () => (
     <Scaffold>
