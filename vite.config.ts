b47d5f66486d5c790f0554314ebc6d424a9b8745
Luis Meneses <luismenesesep@gmail.com>
2024-01-26 12:24:19 -0500

    new dev packages added

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,10 +14,18 @@ export default defineConfig({
       '@modules': path.resolve(__dirname, 'src/modules'),
       '@utilities': path.resolve(__dirname, 'src/utilities'),
       '@components': path.resolve(__dirname, 'src/components'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
       '@root': path.resolve(__dirname, 'src')
     }
   },
-  plugins: [react(), TanStackRouterVite(), EnvironmentPlugin('all')],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    EnvironmentPlugin('all'),
+    svgr({ include: '**/*.svg?react' })
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()]
