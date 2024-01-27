c372ee3a88ba9b9b94200f28eeb8148fd103b388
Luis Meneses <luismenesesep@gmail.com>
2024-01-27 17:55:12 -0500

    callback fixed to support redirect

--- src/core/hooks/use-auth-provider/useFacebookAuthProvider.ts
+++ src/core/hooks/use-auth-provider/useFacebookAuthProvider.ts
@@ -1,12 +1,15 @@
 import { AuthenticationFactory } from '@modules/authentication';
 import { useI18n } from '@modules/i18n';
 
+import { useAuthCallbackHandler } from './useAuthCallbackHandler';
+
 const engine = AuthenticationFactory.create('facebook');
 
 export const useFacebookAuthProvider = () => {
   const { tr } = useI18n();
+  const callbacks = useAuthCallbackHandler(engine);
   return {
-    ...engine,
+    ...callbacks,
     signInLabel: tr('auth:sign-in', { provider: tr('facebook:name') }),
     signUpLabel: tr('auth:sign-up', { provider: tr('facebook:name') }),
     signOutLabel: tr('auth:sign-out', { provider: tr('facebook:name') })
